3f9d4aed3b810bde7875d0f1c3cacca8503b6b13
Arjun Singh <101566727+ArjunSinghBhakuni@users.noreply.github.com>
2024-08-28 16:53:28 +0000

    assignements

---
+++ BD1.5_Assignement_1/index.js
@@ -0,0 +1,76 @@
+
+let express = require("express");
+let cors = require("cors")
+let app = express();
+
+app.use(cors());
+
+app.get("/cart-total",(req , res )=>{
+  console.log("testing")
+  let newItemPrice= parseFloat(req.query.newItemPrice);
+  let cartTotal = paraseFloat(req.query.cartTotal);
+  if(cartTotal==0){
+   res.send(newItemPrice.toString()); 
+  }
+
+  res.send((newItemPrice*cartTotal).toString());
+})
+
+calculateDiscunt = (cartTotal,isMember)=>{
+
+  if(isMember){
+  return( cartTotal - (cartTotal*0.1));
+  }
+  else {
+    return cartTotal;
+  }
+}
+
+app.get("membership-discount",(req , res )=>{
+
+  let cartTotal = parseFloat(req.query.cartTotal);
+  let isMember = req.query.isMember == "true"?true:false;
+res.send(calculateDiscunt(cartTotal,isMember).toString());
+  });
+
+function calculateTax(carTotal){
+  return (cartTotal*0.05);
+}
+app.get("calculate-tax",(req ,res )=>{
+  let cartTotal = parseFloat(req.query.cartTotal);
+
+  let tax = calculateTax(cartTotal);
+  res.send(tax.toString());
+})
+
+calculateDeliveryTime=(method,distance)=>{
+if(method =="standard"){
+  return distance/50;
+}else {
+  return distance/100;
+}
+  
+}
+app.get("estimate-delivery",(req ,res )=>{
+  let shippingMethod = req.query.shippingMethod;
+  let distance = parseFloat(req.query.distance);
+ res.send(calculateDeleiveryTime(shippingMethod,distance).toString());
+  
+})
+
+calculateShippingCost =(weight,distance)=>{
+ return weight * distance * 0.1 ;
+}
+app.get("shipping-cost",(req ,res )=>{
+  let weight = parseFloat(req.query.weight);
+  let distance = parseFloat(req.query.distance);
+
+  res.send(calculateShippingCost(weight,distance).toString())
+})
+
+app.get("loyalty-points",(req,res )=>{
+  let purchaseAmount = parseFloat(req.query.purchaseAmount);
+
+  res.send((purchaseAmount*2).toString());
+})
+app.listen(3000,()=>console.log("Server is running on port 3000"));
---
+++ BD1.5_Assignement_2/index.js
@@ -0,0 +1,70 @@
+let express = require("express");
+let cors = require("cors");
+let app = express();
+let PORT = 3000;
+
+app.use(cors());
+
+app.get("/",(req,res)=>{
+  res.send("Welcome to Stock Market");
+})
+
+app.get("calculate-returns",(req,res)=>{
+  let boughtAt = parseFloat(req.query.boughtAt);
+  let marketPrice = parseFloat(req.query.marketPrice);
+  let quantity = parseFloat(req.query.quantity);
+
+  let returns = (marketPrice - boughtAt) * quantity;
+
+  res.send(returns.toString());
+  
+});
+
+app.get("total-returns",(req,res)=>{
+  let stock1 = parseFloat(req.query.stock1);
+  let stock2 = parseFloat(req.query.stock2);
+  let stock3 = parseFloat(req.query.stock3);
+  let stock4 = parseFloat(req.query.stock4);
+
+  let totalStocks = stock1 + stock2 + stock3 + stock4;
+  res.send(totalStocks.toString());
+  
+})
+
+app.get("calculate-return-percentage",(req,res)=>{
+
+  let boughtAt = parseFloat(req.query.boughtAt);
+  let returns = parseFloat(req.query.returns);
+
+  let percentageOfReturns = (returns / boughtAt) * 100;
+  res.send(percentageOfReturns.toString());
+});
+
+
+app.get("total-return-percentage",(req,res)=>{
+  let stock1 = parseFloat(req.query.stock1);
+  let stock2 = parseFloat(req.query.stock2);
+  let stock3 = parseFloat(req.query.stock3);
+  let stock4 = parseFloat(req.query.stock4);
+
+  let totalStocksReturns = stock1 + stock2 + stock3 + stock4;
+  res.send(totalStocksReturns.toString());
+
+});
+
+app.get("status",(req,res)=>{
+  let returnPercentage = parseFloat(req.query.returnPercentage);
+
+  let status;
+  if(returnPercentage > 0){
+    status = "Profit";
+  }else {
+    status = "Loss";
+  }
+  res.send(status);
+});
+app.get
+app.listen(PORT,()=>{
+  console.log('Server is running on port',PORT);
+});
+
